3092064c269d6748b35e9b8c1b8f963412aa469a
Nikko Roque <106610608+nikkoroque@users.noreply.github.com>
2025-01-10 21:25:19 -0800

    Enhance client UI and update dependencies
    - Added `@radix-ui/react-slot` and `mini-svg-data-uri` to dependencies in `package.json`.
    - Updated Tailwind configuration to include a new plugin for SVG background utilities.
    - Modified the `Hero` component to include a `GridBackground` and additional content for improved user engagement.
    - Updated the layout metadata to reflect the new application title and description.
    - Adjusted the height of the `ContainerScroll` component for better visual presentation.
    - Integrated a `Navbar` component into the Home page for improved navigation.

--- client/src/components/hero/Hero.tsx
+++ client/src/components/hero/Hero.tsx
@@ -2,10 +2,22 @@
 import React from "react";
 import { ContainerScroll } from "../ui/container-scroll-animation";
 import Image from "next/image";
+import GridBackground from "../grid-background/GridBackground";
+import { Button } from "../ui/button";
+import { ArrowRight } from "lucide-react";
 
 const Hero = () => {
   return (
+    <>
+    <div className="relative">
+      {/* Add GridBackground as a background layer */}
+      <div className="absolute inset-0">
+        <GridBackground />
+      </div>
+      <div className="relative z-10">
+        <div className="container px-6 py-6 mx-auto text-center">
     <div className="flex flex-col overflow-hidden">
+      
       <ContainerScroll
         titleComponent={
           <>
@@ -15,6 +27,12 @@ const Hero = () => {
               Effortless Inventory
               </span>
             </h1>
+            <p className="text-gray-500 mb-10 text-2xl">
+            Boost efficiency, cut costs, and make smarter inventory decisions.
+            </p>
+            <Button className="mb-16">
+              Book a demo <ArrowRight />
+            </Button>
           </>
         }
       >
@@ -25,9 +43,13 @@ const Hero = () => {
           width={1400}
           className="mx-auto rounded-2xl object-cover h-full object-left-top"
           draggable={false}
-        />
-      </ContainerScroll>
-    </div>
+          />
+        </ContainerScroll>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
